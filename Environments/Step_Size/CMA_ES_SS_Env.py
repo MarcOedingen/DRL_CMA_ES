f424e9bbeae8161b2c24098b7c9fc14724f81869
MarcUnknown <marc.oedingen.ta12@gmx.de>
2023-12-20 17:13:32 +0100

    Value-based experiments according to Shala

--- Environments/Step_Size/CMA_ES_SS_Env.py
+++ Environments/Step_Size/CMA_ES_SS_Env.py
@@ -1,12 +1,9 @@
+import torch
 import gymnasium
 import numpy as np
+import tensorflow as tf
 from collections import deque
 from Environments.Step_Size.CMA_ES_SS import CMAES
-
-
-def _norm(x):
-    return np.sqrt(np.sum(np.square(x)))
-
 
 class CMA_ES_SS(gymnasium.Env):
     def __init__(self, objetive_funcs, sigma):
@@ -22,11 +19,12 @@ class CMA_ES_SS(gymnasium.Env):
         self.hist_fit_vals = deque(np.zeros(self.h), maxlen=self.h)
         self.hist_sigmas = deque(np.zeros(self.h), maxlen=self.h)
 
-        self.action_space = gymnasium.spaces.Box(low=1e-5, high=1, shape=(1,), dtype=np.float32)
-        self.observation_space = gymnasium.spaces.Box(low=0, high=5, shape=(2 + 2 * self.h,))
+        self.action_space = gymnasium.spaces.Box(low=1e-10, high=10, shape=(1,), dtype=np.float64)
+        self.observation_space = gymnasium.spaces.Box(low=-np.inf, high=np.inf, shape=(2 + 2 * self.h,), dtype=np.float64)
 
         self.iteration = 0
         self._stop = False
+        self._f_limit = np.power(10, 36)
 
     def step(self, action):
         self.curr_sigma = action[0]
@@ -37,8 +35,9 @@ class CMA_ES_SS(gymnasium.Env):
         fit = [self.objetive_funcs[self.curr_index](x) for x in X]
         new_ps = self.cma_es.tell(X, fit)
 
-        # Calculate reward (Turning maximization into minimization)
-        reward = -np.mean(fit)
+        # Calculate reward (Turn minimization into maximization)
+        reward = -np.min(fit)
+        reward = np.clip(reward, -self._f_limit, self._f_limit)
 
         # Check if the algorithm should stop
         # Terminated if all functions have been evaluated
@@ -49,12 +48,10 @@ class CMA_ES_SS(gymnasium.Env):
         # Update history
         # If self.iteration is greater than 0, then the difference between the current reward and the previous reward is added to the history
         if self.iteration > 0:
-            self.hist_fit_vals.append(abs(reward - self.hist_fit_vals[len(self.hist_fit_vals) - 1]))
+            self.hist_fit_vals.append(np.abs((reward - self.hist_fit_vals[len(self.hist_fit_vals) - 1])) / reward)
             self.hist_sigmas.append(self.curr_sigma)
 
-        new_state = np.concatenate(
-            [np.array([self.curr_sigma]), np.array([_norm(new_ps) / self.cma_es.params.chiN - 1]),
-             np.array(self.hist_fit_vals), np.array(self.hist_sigmas)], dtype=np.float32)
+        new_state = np.concatenate([np.array([self.curr_sigma]), np.array([np.linalg.norm(new_ps) / self.cma_es.params.chiN - 1]), np.array(self.hist_fit_vals), np.array(self.hist_sigmas)])
 
         # Update current ps
         self.curr_ps = new_ps
@@ -69,19 +66,17 @@ class CMA_ES_SS(gymnasium.Env):
         # Update iteration
         self.iteration += 1
 
-        '''print("Iteration: ", self.iteration, " Reward: ", reward, " Sigma: ", self.curr_sigma, " PS: ", self.curr_ps,
-              " Terminated: ", terminated, " Truncated: ", truncated)'''
-
         return new_state, reward, terminated, truncated, {}
 
-    def reset(self, *, seed=None, options=None):
-        print(f"Evaluating new function: {self.curr_index + 1}/{len(self.objetive_funcs)}")
+    def reset(self, *, seed=None, options=None, verbose=1):
+        if verbose > 0 and self.curr_index < len(self.objetive_funcs):
+            print(f"Training on function {self.objetive_funcs[self.curr_index % len(self.objetive_funcs)].function} | {(self.curr_index / len(self.objetive_funcs)) * 100}% done")
         self.curr_sigma = self.sigma
         self.curr_ps = 0
         self.hist_fit_vals = deque(np.zeros(self.h), maxlen=self.h)
         self.hist_sigmas = deque(np.zeros(self.h), maxlen=self.h)
-        self.cma_es = CMAES(np.random.uniform(low=-5, high=5, size=self.objetive_funcs[self.curr_index].dimension),
-                            self.sigma)
+        #self.cma_es = CMAES(np.random.uniform(low=-5, high=5, size=self.objetive_funcs[self.curr_index % len(self.objetive_funcs)].dimension), self.sigma)
+        self.cma_es = CMAES(np.zeros(self.objetive_funcs[self.curr_index % len(self.objetive_funcs)].dimension), self.sigma)
         self.iteration = 0
         return np.concatenate(
             [np.array([self.curr_sigma]), np.array([self.curr_ps]), list(self.hist_fit_vals),
@@ -89,5 +84,3 @@ class CMA_ES_SS(gymnasium.Env):
 
     def render(self):
         pass
-
-
--- Environments/Step_Size/CMA_ES_SS_run.py
+++ Environments/Step_Size/CMA_ES_SS_run.py
@@ -1,30 +1,47 @@
-import gymnasium
+import numpy as np
 from stable_baselines3 import PPO
 from cocoex.function import BenchmarkFunction
+from sklearn.model_selection import train_test_split
 from Environments.Step_Size.CMA_ES_SS_Env import CMA_ES_SS
+from stable_baselines3.common.callbacks import BaseCallback
 
-class CustomStopTrainingException(Exception):
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
+class StopOnAllFunctionsEvaluated(BaseCallback):
+    def __init__(self, verbose=0):
+        super(StopOnAllFunctionsEvaluated, self).__init__(verbose)
+        self._stop = False
 
-class StopTrainingWrapper(gymnasium.Wrapper):
-    def reset(self, **kwargs):
-        if self.env._stop:
-            raise CustomStopTrainingException("All functions evaluated")
-        return self.env.reset(**kwargs)
-
+    def _on_step(self) -> bool:
+        if self.model.env.envs[0].env._stop:
+            return False
+        return True
 
 
 def main():
     dim = 10
-    funcs = [BenchmarkFunction("bbob", i, dim, 1) for i in range(1, 25)]
+    train_ids, test_ids = train_test_split(np.arange(1, 25), test_size=0.2, random_state=42)
+    train_funcs = np.repeat([BenchmarkFunction("bbob", train_id, dim, 1) for train_id in train_ids], 20)
+    np.random.shuffle(train_funcs)
+    test_funcs = [BenchmarkFunction("bbob", test_id, dim, 1) for test_id in test_ids]
+    test_funcs = np.repeat(test_funcs, 10)
 
-    env = CMA_ES_SS(objetive_funcs=funcs, sigma=0.5)
-    env = StopTrainingWrapper(env)
+    train_env = CMA_ES_SS(objetive_funcs=train_funcs, sigma=0.5)
+    ppo_model = PPO("MlpPolicy", train_env, verbose=0)
+    ppo_model.learn(total_timesteps=int(1e6), callback=StopOnAllFunctionsEvaluated())
 
-    ppo_model = PPO("MlpPolicy", env, verbose=0)
-    ppo_model.learn(total_timesteps=int(1e6))
+    # Evaluate the trained agent on the test functions
+    rewards = np.zeros(len(test_funcs))
+    for index, test_func in enumerate(test_funcs):
+        eval_env = CMA_ES_SS(objetive_funcs=[test_func], sigma=0.5)
+        obs, _ = eval_env.reset(verbose=0)
+        terminated, truncated = False, False
+        steps = 0
+        while not (terminated or truncated):
+            action, _states = ppo_model.predict(obs, deterministic=False)
+            obs, reward, terminated, truncated, info = eval_env.step(action)
+            steps += 1
+        rewards[index] = -reward
 
+    for i in range(len(test_funcs)):
+        print("Function: ", test_funcs[i].function, " Reward: ", rewards[i], "Optimum: ", test_funcs[i].best_value())
 
 main()
