5467de3fab2c9c87bbe66293d94fc4003018b778
MarcOedingen <marc.oedingen.ta12@gmx.de>
2023-12-27 17:55:31 +0100

    Add correct description for x-values

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     parser = argparse.ArgumentParser(description='Run different CMA-ES algorithms')
     parser.add_argument('--algorithm', type=str, help='The dataset to use', choices=['baseline', 'stepsize'],default='stepsize')
     parser.add_argument('--dimension', type=int, help='The dimension of the problem', default=2)
-    parser.add_argument('--xstart', type=str, help='The dataset to use', choices=['zero', 'random'],default='zero')
+    parser.add_argument('--xstart', type=str, help='The x-values starting from', choices=['zero', 'random'],default='zero')
     parser.add_argument('--sigma', type=float, help='The initial sigma', default=0.5)
     parser.add_argument('--instance', type=int, help='The instance of the problem', default=1)
 
@@ -18,8 +18,6 @@ def main():
     elif args.algorithm == 'stepsize':
         from Environments.Step_Size.CMA_ES_SS_run import run
         run(args.dimension, args.xstart, args.sigma, args.instance)
-
-
 
 if __name__ == '__main__':
     main()
