ba71d4b3110c5e7df335fe29ded93c231a418f75
MarcUnknown <marc.oedingen.ta12@gmx.de>
2024-01-22 12:51:14 +0100

    Change observation to reflect differences not divided by the last achieved one

--- Environments/Step_Size/CMA_ES_SS.py
+++ Environments/Step_Size/CMA_ES_SS.py
@@ -29,7 +29,7 @@ def run_CMAES_SS(objective_fct, x_start, sigma, h=40, f_limit=np.power(10, 28)):
                 -f_limit,
                 f_limit,
             )
-            hist_fit_vals.append(difference / np.log(np.abs(f_best)))
+            hist_fit_vals.append(difference)
         hist_sigmas.append(curr_sigma)
         observations.append(
             np.concatenate(
